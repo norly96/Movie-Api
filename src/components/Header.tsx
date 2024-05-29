6731bd2001dced19f897951d0d528f4fa49ccef0
Norluis Galvez <norly021196@gmail.com>
2024-05-29 03:45:51 -0400

    add new style and add my icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,26 +8,28 @@ import {
 const Header = () => {
   return (
     <header className="w-full flex justify-between px-16 py-4 mx-auto ">
-      <button className="h-12 px-3 py-2 text-md font-bold text-white uppercase bg-[#c44900] rounded">
-        <a href="" className="flex flex-row items-center">
+      <button className="h-12 px-3 py-2 text-md font-bold text-white uppercase bg-[#63e] rounded">
+        <p className="flex flex-row items-center">
           <IconMovie size={27} />
           MovieApi
-        </a>
+        </p>
       </button>
 
       <nav className="flex flex-wrap items-center gap-2 justify-end pl-6 ml-6 border-l text-white border-gray-200 ">
+        <a href="">
+          <img
+            src="/public/blanco.png"
+            alt=""
+            className="w-6 transition-transform duration-500 hover:scale-125 "
+          />
+        </a>
         <a
           href="#_"
           className="transition-transform duration-500 hover:scale-125 "
         >
           <IconBrandGithub size={30} />
         </a>
-        <a
-          href="#_"
-          className="transition-transform duration-500 hover:scale-125 "
-        >
-          <IconBrandInstagram size={30} />
-        </a>
+
         <a
           href="#_"
           className="transition-transform duration-500 hover:scale-125 "
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,14 +6,14 @@ const Search = () => {
       <h1 className="text-white text-center text-5xl sm:text-6xl font-bold">
         Explore the different movies...
       </h1>
-      <div className="flex w-full sm:w-2/3 mt-12 text-white bg-[#232342] rounded-2xl border-2 border-slate-500">
+      <div className="flex w-full sm:w-2/3 mt-12 text-white bg-black rounded-2xl border-2 border-slate-700">
         <IconSearch stroke={2} size={30} className="ml-2 my-auto" />
         <input
           type="text"
-          className="w-full text-lg py-2 px-2  focus:outline-none bg-[#232342]"
+          className="w-full text-lg py-2 px-2  focus:outline-none bg-black"
         />
 
-        <button className=" bg-[#c44900] px-2 sm:px-6 rounded-xl m-0.5">
+        <button className=" bg-[#63e] hover:bg-violet-500 duration-300 px-2 sm:px-6 rounded-xl m-0.5">
           Search
         </button>
       </div>
